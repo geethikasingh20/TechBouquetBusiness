6ae8982cf5a78666ea436da99f7d57fecb0bb2cd
Geetika <geethika.singh20>
2026-03-21 01:36:11 +0530

    added render domain in CORS

--- backend/src/main/java/com/techbouquet/config/WebConfig.java
+++ backend/src/main/java/com/techbouquet/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("http://localhost:5173","https://techbouquetbusiness.onrender.com/")
                         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
