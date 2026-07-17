03ab67009c52665e9badef3210365636f71502ad
Geetika <geethika.singh20>
2026-07-17 17:34:21 +0530

    changed the carousel

--- frontend/src/components/Carousel.jsx
+++ frontend/src/components/Carousel.jsx
@@ -2,7 +2,9 @@ import { useEffect, useState } from "react";
 import slide1 from "../assets/carousel1.png";
 import slide2 from "../assets/carousel2.jpg";
 
-const slides = [slide1,slide2];
+import slide6 from "../assets/c6.jpg";
+
+const slides = [slide5, slide6];
 
 export default function Carousel() {
   const [index, setIndex] = useState(0);
